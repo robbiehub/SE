7f90e72b17d9474d962e141daf4e48d5876e7066
Luis Roberto Alcazar Ortega <luisrob.alcazar@gmail.com>
2016-01-18 00:51:08 -0700

    Mas progreso
    *Se agregaron las siguientes funciones a recommendations.py:
    ->calculateSimilarItems
    ->getRecommendedItems
    
    *Se creó el archivo deliciousrec.py, esto nos permite hacer operaciones
    con informacion de Delicious.
    NOTAS:
    ->Se necesita tener instalado pydelicious para que funcione, el link lo
    puedes encontrar en el pdf.
    ->Tuve que modificar el archvio _init_.py de pydelicious para que me
    funcionara, el ultimo post de este link te dice que modificar
    (http://stackoverflow.com/questions/29543799/pydelicious-get-popularprogramming-doesnt-return-any-valid-url)
    
    Nomas faltaria agregar lo de MovieLens.

--- recommendations.py
+++ recommendations.py
@@ -53,7 +53,7 @@ def sim_pearson(prefs,p1,p2):
 # Number of results and similarity function are optional params.
 def topMatches(prefs,person,n=5,similarity=sim_pearson):
     scores=[(similarity(prefs,person,other),other)
-            for other in prefs if other!=person]
+                for other in prefs if other!=person]
     # Sort the list so the highest scores appear at the top
     scores.sort( )
     scores.reverse( )
@@ -83,11 +83,69 @@ def getRecommendations(prefs,person,similarity=sim_pearson):
                 # Sum of similarities
                 simSums.setdefault(item,0)
                 simSums[item]+=sim
-
     # Create the normalized list
     rankings=[(total/simSums[item],item) for item,total in totals.items( )]
 
     # Return the sorted list
+    rankings.sort()
+    rankings.reverse()
+    return rankings
+
+##### Swapperino #####
+#En esta funcion se cambia la lista de criticos por una lista de peliculas.
+def transformPrefs(prefs):
+    result={}
+    for person in prefs:
+        for item in prefs[person]:
+            result.setdefault(item,{})
+            # Flip item and person
+            result[item][person]=prefs[person][item]
+    return result
+
+##### Construir un dataset para comparar items #####
+def calculateSimilarItems(prefs,n=10):
+    # Create a dictionary of items showing which other items they
+    # are most similar to.
+    result={}
+
+    # Invert the preference matrix to be item-centric
+    itemPrefs=transformPrefs(prefs)
+    c=0
+    for item in itemPrefs:
+        # Status updates for large datasets
+        c+=1
+        if c%100==0: print "%d / %d" % (c,len(itemPrefs))
+        # Find the most similar items to this one
+        scores=topMatches(itemPrefs,item,n=n,similarity=sim_distance)
+        result[item]=scores
+    return result
+
+##### Recomienda items usando el dataset de la funcion anterior. #####
+def getRecommendedItems(prefs,itemMatch,user):
+    userRatings=prefs[user]
+    scores={}
+    totalSim={}
+
+    # Loop over items rated by this user
+    for (item,rating) in userRatings.items( ):
+
+        # Loop over items similar to this one
+        for (similarity,item2) in itemMatch[item]:
+            # Ignore if this user has already rated this item
+            if item2 in userRatings: continue
+
+            # Weighted sum of rating times similarity
+            scores.setdefault(item2,0)
+            scores[item2]+=similarity*rating
+
+            # Sum of all the similarities
+            totalSim.setdefault(item2,0)
+            totalSim[item2]+=similarity
+
+    # Divide each total score by total weighting to get an average
+    rankings=[(score/totalSim[item],item) for item,score in scores.items( )]
+
+    # Return the rankings from highest to lowest
     rankings.sort( )
     rankings.reverse( )
     return rankings
